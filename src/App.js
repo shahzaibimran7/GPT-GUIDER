0fca52afb025417757b2a3bfd3a291a41516c0d0
saqib <saqibb07@github.com>
2023-09-29 20:57:58 +0500

    icons not showing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import Card from './components/Card'
+import Feature from './Pages/Feature'
+// import Card from './components/Card'
 // import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <div className="bg-[#07091E] h-screen w-screen pt-6 px-[140px]">
+    <>
+      {/* <div className="bg-[#07091E] h-screen w-screen pt-6 px-[140px]">
       {/* <Navbar /> */}
-      <Card title="abc" icon="icon" description="lorem10djhdbcbjhabcdjbcsdcsdhg" />
-    </div>
+      {/* </div> */}
+      <div className="bg-[#07091E]">
+        <Feature />
+      </div>
+    </>
   )
 }
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,39 @@
+// import React from 'react'
+
+// const Card = props => {
+//   return (
+//     <div className="w-[20rem] border-[ 2px solid var(--blue, #642CB6)] h-[18.75rem] text-white bg-stone-950 rounded-[3.125rem] border-3 p-2  flex flex-col justify-center alig items-center  " style={{ border: '2px solid var(--blue, #642CB6)' }}>
+//       <div className="w-[5.25rem] h-[5.25rem] rounded-[10.5rem] bg-[#3A1078] blur-[34px] relative">
+//         <div className="h-[2.625rem] w-[2.625] z-10 absolute">{props.icon}</div>
+//       </div>
+//       <div>
+//         <div className="leading-8 font-black py-[1rem] text-base text-center fontbold">{props.title}</div>
+//         <div className="text-center  font-medium text-base" style={{ width: '17.375rem' }}>
+//           {props.description}
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Card
 import React from 'react'
 
 const Card = props => {
   return (
-    <div className="w-[22.5rem] border-[ 2px solid var(--blue, #642CB6)] h-[18.75rem] text-white bg-stone-950 rounded-[3.125rem] border-3 p-2  flex flex-col justify-center alig items-center  " style={{ border: '2px solid var(--blue, #642CB6)' }}>
-      <div className="">{props.icon}</div>
-      <div className="font-black py-[1rem]">{props.title}</div>
-      <div className="text-center">{props.description}</div>
+    <div className="w-[20rem] border-2 border-blue-500 h-[18.75rem] text-white bg-stone-950 rounded-[3.125rem] p-2 flex flex-col justify-center items-center">
+      <div className="w-[5.25rem] h-[5.25rem] rounded-[10.5rem] bg-[#3A1078] blur-[34px] relative">
+        <div className="h-[2.625rem] w-[2.625rem] z-10 absolute top-0 left-0">
+          {/* Display the icon */}
+          {props.icon && <img src={props.icon} alt="Icon" />}
+        </div>
+      </div>
+      <div>
+        <div className="leading-8 font-black py-[1rem] text-base text-center font-bold">{props.title}</div>
+        <div className="text-center font-medium text-base" style={{ width: '17.375rem' }}>
+          {props.description}
+        </div>
+      </div>
     </div>
   )
 }
